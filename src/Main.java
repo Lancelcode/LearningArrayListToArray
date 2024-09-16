c262e2131977a6b6ae2c63e3d77bba9e05d2dc92
Lancelcode <sowrebollo@gmial.com>
2024-09-16 17:42:23 +0100

    Learning ArrayList

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args)
+    {
+        ArrayList<String> namesList = new ArrayList<>();
+
+        namesList.add("Alice");
+        namesList.add("Bob");
+        namesList.add("Charlie");
+        namesList.add("Derek");
+
+        System.out.println(namesList);
+    }
+}
